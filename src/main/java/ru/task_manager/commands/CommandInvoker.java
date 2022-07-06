c6efab24cdb1c5e7f1d86e5218d03d6bb3b7e34d
DmShevchuk <dmitriishevchuk2003@yandex.ru>
2022-07-06 21:15:03 +0300

    Add CommandInvoker to run commands, change logic

---
+++ src/main/java/ru/task_manager/commands/CommandInvoker.java
@@ -0,0 +1,36 @@
+package ru.task_manager.commands;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.task_manager.exceptions.*;
+
+import java.util.Arrays;
+
+/**
+ * Класс для вызова команд {@link Command}
+ * */
+@Component
+@RequiredArgsConstructor
+public class CommandInvoker {
+    private final CommandFactory commandFactory;
+    private final int COMMAND_NAME_INDEX = 0;
+
+    public String invoke(String[] args) {
+        try {
+            Command command = commandFactory.getCommand(args[COMMAND_NAME_INDEX]);
+            command.resetArgs();
+            if (args.length > 1) {
+                command.setArgs(Arrays.copyOfRange(args, 1, args.length));
+            }
+            return command.execute();
+        } catch (IncorrectCommandException |
+                CommandExecutionException |
+                IncorrectArgsQuantityException |
+                FieldParseException |
+                UserNotFoundException |
+                UserAlreadyExistsException |
+                TaskNotFoundException e) {
+            return e.getMessage();
+        }
+    }
+}
--- src/main/java/ru/task_manager/controllers/MainController.java
+++ src/main/java/ru/task_manager/controllers/MainController.java
@@ -4,16 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.task_manager.commands.CommandInvoker;
 import ru.task_manager.utils.LineHandler;
+
+import java.text.ParseException;
 
 /**
  * Главный контроллер, принимающий все запросы от пользователя
- * */
+ */
 @RestController
 @RequiredArgsConstructor
 public class MainController {
-    private final LineHandler lineParser;
-
+    private final LineHandler lineHandler;
+    private final CommandInvoker commandInvoker;
     /**
      * Примеры задания запросов:<br/>
      * http://localhost:8080/?line=add_user,Ivan Petrov <br/>
@@ -22,14 +25,14 @@ public class MainController {
      * http://localhost:8080/?line=show_users <br/>
      * http://localhost:8080/?line=change_task_by_id, 1, Do homework, Write a lot of code, 04.07.2022, done, 1 <br/>
      * http://localhost:8080/?line=delete_task_by_id,1 <br/>
-     * */
+     */
     @GetMapping("/")
     public String index(@RequestParam String line) {
         try {
-            return lineParser.parse(line.strip());
-        }catch (Exception e){
+            String[] args = lineHandler.parse(line.strip());
+            return commandInvoker.invoke(args);
+        } catch (Exception e) {
             return e.getMessage();
         }
-
     }
 }
