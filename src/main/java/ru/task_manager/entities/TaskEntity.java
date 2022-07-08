ee40258302ac200286a1291ef691a0df1e189d3c
DmShevchuk <dmitriishevchuk2003@yandex.ru>
2022-07-08 05:12:44 +0300

    Add builder in TaskFactory

--- src/main/java/ru/task_manager/entities/TaskEntity.java
+++ src/main/java/ru/task_manager/entities/TaskEntity.java
@@ -25,4 +25,55 @@ public class TaskEntity {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private UserEntity user;
+
+    private TaskEntity(Long id, String title, String description, Date deadline, TaskType type) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.type = type;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String title;
+        private String description;
+        private Date deadline;
+        private TaskType taskType;
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setDeadline(Date deadline) {
+            this.deadline = deadline;
+            return this;
+        }
+
+        public Builder setTaskType(TaskType taskType) {
+            this.taskType = taskType;
+            return this;
+        }
+
+        public TaskEntity build() {
+            return new TaskEntity(
+                    id,
+                    title,
+                    description,
+                    deadline,
+                    taskType
+            );
+        }
+    }
 }
