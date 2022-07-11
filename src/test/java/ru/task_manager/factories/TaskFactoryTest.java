2163f6f4858d6e97691e004ce24cd57f29698b71
DmShevchuk <dmitriishevchuk2003@yandex.ru>
2022-07-11 03:37:16 +0300

    Add tests for TaskFactory

---
+++ src/test/java/ru/task_manager/factories/TaskFactoryTest.java
@@ -0,0 +1,88 @@
+package ru.task_manager.factories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.task_manager.entities.TaskEntity;
+import ru.task_manager.exceptions.FieldParseException;
+import ru.task_manager.utils.InputParser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskFactoryTest {
+    private InputParser inputParser;
+    private TaskFactory taskFactory;
+
+    @BeforeEach
+    void setUp(){
+        this.inputParser = new InputParser();
+        this.taskFactory = new TaskFactory(inputParser);
+    }
+
+    @Test
+    @DisplayName("Get TaskEntity with invalid date")
+    void getTaskEntityWithInvalidDate() {
+        String[] fields = new String[]{
+                "Title",
+                "Description",
+                "String instead of date",
+                "done"
+        };
+
+        assertThrows(FieldParseException.class, () -> taskFactory.getTaskEntity(fields));
+    }
+
+    @Test
+    @DisplayName("Get TaskEntity with correct fields")
+    void getTaskEntityWithValidData() {
+        String[] fields = new String[]{
+                "Title",
+                "Description",
+                "11.07.2022",
+                "done"
+        };
+        TaskEntity taskEntity = new TaskEntity.Builder()
+                .setTitle(inputParser.parseString(fields[0]))
+                .setDescription(inputParser.parseString(fields[1]))
+                .setDeadline(inputParser.parseDate(fields[2]))
+                .setTaskType(inputParser.parseTaskType(fields[3]))
+                .build();
+        assertEquals(taskEntity.getId(), taskFactory.getTaskEntity(fields).getId());
+    }
+
+
+    @Test
+    @DisplayName("Update TaskEntity with invalid id")
+    void updateTaskEntityWithInvalidId() {
+        String[] fields = new String[]{
+                "String instead of id",
+                "Title",
+                "Description",
+                "11.07.2022",
+                "done"
+        };
+
+        assertThrows(FieldParseException.class, () -> taskFactory.updateTaskEntity(fields));
+    }
+
+    @Test
+    @DisplayName("Update TaskEntity with valid id")
+    void updateTaskEntityWithValidId() {
+        String[] fields = new String[]{
+                "1",
+                "Title",
+                "Description",
+                "11.07.2022",
+                "done"
+        };
+        TaskEntity taskEntity = new TaskEntity.Builder()
+                .setId(inputParser.parseLong(fields[0]))
+                .setTitle(inputParser.parseString(fields[1]))
+                .setDescription(inputParser.parseString(fields[2]))
+                .setDeadline(inputParser.parseDate(fields[3]))
+                .setTaskType(inputParser.parseTaskType(fields[4]))
+                .build();
+
+        assertEquals(taskEntity.getId(), taskFactory.updateTaskEntity(fields).getId());
+    }
+}
